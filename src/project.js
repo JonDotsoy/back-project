bc046e933e6f28e60ad0d3611d1d533620a1c87a
Jon Dotsoy <hi@jon.soy>
2015-06-29 00:55:44 +0100

    -

--- src/project.js
+++ src/project.js
@@ -1,11 +1,13 @@
+'use strict'
 /**
  * Project Manager
  */
 
 var path = require('path')
+var comfig = require(path.join(__dirname, 'config.js'))
 
 module.exports = {
   init: function () {
     // Scan directorio git
   },
-};
+}
