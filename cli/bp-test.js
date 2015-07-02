f3067554af991bc411b752a2e79d9bed46e8da35
Jon Dotsoy <hi@jon.soy>
2015-07-02 09:24:52 +0100

    prepare next version

--- cli/bp-test.js
+++
@@ -1,5 +0,0 @@
-var path = require("path")
-var config = require(path.join(__dirname, "src", "config.js"))
-var bpproject = require(path.join(__dirname, 'src', 'project.js'))
-var command = require('commander')
-
